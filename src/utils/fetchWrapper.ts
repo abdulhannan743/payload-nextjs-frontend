033f277469f7b28809b2ea8241dddb874f5edac0
dilawarkhankakar <137504787+dilawarkhankakar@users.noreply.github.com>
2024-07-01 19:53:59 +0500

    fixed fetchWrapper

--- src/utils/fetchWrapper.ts
+++ src/utils/fetchWrapper.ts
@@ -1,73 +1,59 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 /**
  * @function handleResponse
  * @param {Object} response - The response object.
  * @description
- *   A handler for the fetch response Object
+ *   A handler for the axios response Object
  * @return {Promise<T>} A promise containing the deserialized response body.
- * */
+ */
 export async function handleResponse<TData>(
-  response: Response
+  response: AxiosResponse
 ): Promise<TData> {
   if (response.status === 204) {
     return {} as TData;
   }
-
-  const res = await response.json();
-
   if (response.status < 200 || response.status >= 300) {
     throw new Error(
       `There has been an error. Response status: ${response.status}`
     );
   }
-
-  return res;
+  return response.data as TData;
 }
 
+/**
+ * Axios wrapper function for making API calls.
+ * @param {Object} config - Configuration object for the request.
+ * @returns {Promise<TData>} - A promise containing the response data.
+ */
 export async function fetchWrapper<TData, TBody = unknown>({
   method = "GET",
   url,
   body,
-  token,
   additionalHeaders,
   ...additionalOptions
 }: {
   url: string;
   method?: string;
   body?: TBody;
-  token?: string;
-  additionalHeaders?: {
-    Accept: string;
-    "Content-Type": string;
-  };
-  additionalOptions?: any;
+  additionalHeaders?: Record<string, string>;
+  additionalOptions?: AxiosRequestConfig;
 }): Promise<TData> {
   const API_BASEURL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
   const _url = `${API_BASEURL}${url}`;
-  const options = {
+  const options: AxiosRequestConfig = {
     method: method,
+    url: _url,
     headers: {
-      Accept: additionalHeaders?.Accept || "application/vnd.api+json ",
-      "Content-Type":
-        additionalHeaders?.["Content-Type"] || "application/vnd.api+json",
+      "Accept": additionalHeaders?.Accept || "application/json",
+      "Content-Type": additionalHeaders?.["Content-Type"] || "application/json",
+      ...additionalHeaders,
     },
-    credentials: "include" as RequestCredentials,
-    body: body && JSON.stringify(body), // body can be undefined, that's ok
+    withCredentials: true,
+    data: body,
     ...additionalOptions,
   };
 
-  const response = await fetch(_url, options);
+  const response = await axios(options);
   return await handleResponse<TData>(response);
 }
-
-// import axios from "axios";
-
-// export async function fetchWrapper(url: string) {
-//   try {
-//     const response = await axios.get(`${url}`);
-//     return response.data;
-//   } catch (error) {
-//     console.error("Error fetching header data:", error);
-//     throw error;
-//   }
-// }
