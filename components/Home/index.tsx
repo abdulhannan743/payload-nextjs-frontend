c10115a53c281c343e228bf4dfbd3b15420d54bf
dilawarkhankakar <137504787+dilawarkhankakar@users.noreply.github.com>
2024-07-08 16:36:27 +0500

    testimonials section

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ServicesSection from "./ServicesSection";
 import WhyAllZoneSection from "./WhyAllZoneSection";
+import ClientsSection from "./ClientsSection";
 
 type HomeProps = {
   homePageData: any;
@@ -13,10 +14,14 @@ export default function Home({ homePageData }: HomeProps) {
   const whyAllzoneData = homePageData?.find(
     (item: any) => item?.blockName === "Why Allzone Section"
   );
+  const clientsData = homePageData?.find(
+    (item: any) => item?.blockName === "Our Clients"
+  );
   return (
     <div className="bg-white">
       <ServicesSection serviceData={servicesData} />
       <WhyAllZoneSection whyAllzoneData={whyAllzoneData} />
+      <ClientsSection clientsData={clientsData} />
     </div>
   );
 }
