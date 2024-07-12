ff043aabba87a1300164f892349a801cffed2bbc
atii6 <atif.razzaq@allzonetech.com>
2024-07-12 15:56:22 +0500

    Fixed Minor Error

--- components/About/CoreValues.tsx
+++ components/About/CoreValues.tsx
@@ -156,9 +156,9 @@ const CoreValuesSection = ({
             className="size-6 w-10 h-10"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M7.28 7.72a.75.75 0 0 1 0 1.06l-2.47 2.47H21a.75.75 0 0 1 0 1.5H4.81l2.47 2.47a.75.75 0 1 1-1.06 1.06l-3.75-3.75a.75.75 0 0 1 0-1.06l3.75-3.75a.75.75 0 0 1 1.06 0Z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
             />
           </svg>
         </div>
@@ -172,13 +172,13 @@ const CoreValuesSection = ({
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             className="size-6 w-10 h-10"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
             />
           </svg>
